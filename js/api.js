234ec18b1cd3267c41583065a1a0bac93196c82d
Carlos Flores Siordia <136644568+CarlosSiordia@users.noreply.github.com>
2026-07-28 02:26:17 -0600

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,77 +1,23 @@
-const API_URL = "https://api.pokemontcg.io/v2/cards";
+const API_URL = "https://api.pokemontcg.io/v2/cards?q=name:Spiritomb";
 
 
-async function searchSpiritomb(){
-
-
-    const debug = document.createElement("div");
-
-    debug.id="debug";
-
-    debug.style.position="fixed";
-
-    debug.style.bottom="10px";
-
-    debug.style.left="10px";
-
-    debug.style.right="10px";
-
-    debug.style.padding="15px";
-
-    debug.style.background="#222";
-
-    debug.style.color="white";
-
-    debug.style.zIndex="9999";
-
-    document.body.appendChild(debug);
-
-
+async function getSpiritombCards(){
 
     try{
 
+        const response = await fetch(API_URL);
 
-        debug.innerHTML =
-        "Conectando con API...";
-
-
-
-        const response = await fetch(
-            `${API_URL}?q=name:Spiritomb`
-        );
-
-
-        debug.innerHTML +=
-        "<br>Respuesta: "+response.status;
-
-
-
-        const data =
-        await response.json();
-
-
-
-        debug.innerHTML +=
-        "<br>Cartas encontradas: "
-        + data.data.length;
-
-
+        const data = await response.json();
 
         return data.data;
 
 
-
     }catch(error){
 
-
-        debug.innerHTML =
-        "ERROR: "
-        + error.message;
-
+        console.log(error);
 
         return [];
 
     }
-
 
 }
