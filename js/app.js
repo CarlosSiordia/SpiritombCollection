862de1533a54410bc3e3de1391a8f01e0605eab0
Carlos Flores Siordia <136644568+CarlosSiordia@users.noreply.github.com>
2026-07-28 01:21:38 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -3,3 +3,18 @@ window.onload = () => {
     createCards(cards);
 
 };
+
+if("serviceWorker" in navigator){
+
+navigator.serviceWorker
+.register("sw.js")
+
+.then(()=>{
+
+console.log(
+"Spiritomb App instalada correctamente"
+);
+
+});
+
+}
