c62ebc096ae4c7b93da755a6cd7b6758f001e61b
Carlos Flores Siordia <136644568+CarlosSiordia@users.noreply.github.com>
2026-07-28 01:37:11 -0600

    Update cards.js

--- js/cards.js
+++ js/cards.js
@@ -1,35 +1,59 @@
 const cards = [
 
+
 {
 id:1,
+
 pokemon:"Spiritomb",
+
 set:"Legends Awakened",
+
 number:"16/146",
+
 year:2008,
+
 rarity:"Rare",
+
 image:"assets/images/legends_awakened.png"
+
 },
+
 
 
 {
 id:2,
+
 pokemon:"Spiritomb",
+
 set:"Supreme Victors",
+
 number:"84/147",
+
 year:2009,
+
 rarity:"Rare",
+
 image:"assets/images/supreme_victors.png"
+
 },
+
 
 
 {
 id:3,
+
 pokemon:"Spiritomb",
+
 set:"Arceus",
+
 number:"32/99",
+
 year:2009,
+
 rarity:"Rare",
+
 image:"assets/images/arceus.png"
+
 }
 
 
